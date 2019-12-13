16ebd864d73a8b408e716e799e782744ab328c81
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2019-12-14 00:44:39 +0700

    Clean code

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
 const calculator = document.querySelector(".calculator");
 const result = document.querySelector(".result");
-const operators = ["/", "x", "-", "+", "^", "mod", ".", "%"];
+const operators = ["+", "-", "/", "x", "^", "mod", ".", "%"];
 calculator.addEventListener("click", function (e) {
     let resultSpan = result.querySelector("span");
     let clickSpan = e.target.tagName === 'DIV' ? e.target.querySelector("span") : e.target; //pilih span (cek yang ke-klik)
     if (clickSpan.parentElement.className !== 'result') { // cek kalo bukan si result yang dipencet
       let last = resultSpan.innerHTML[resultSpan.innerHTML.length - 1];
       if (operators.indexOf(clickSpan.innerHTML) < 0) { //angka yang dipencet (bukan operator)
-        resultSpan.innerHTML += clickSpan.innerHTML; //tambahin string di belakang result
         if (clickSpan.innerHTML === '=') {
-          resultSpan.innerHTML = resultSpan.innerHTML.substring(0, resultSpan.innerHTML.length - 1); //kurangin angka plg belakang
           resultSpan.innerHTML = resultSpan.innerHTML.replace("x", "*").replace("^", "**").replace("%", "*0.01").replace("mod", "%").replace("π", Math.PI.toString()); //replace operator dengan operator asli
           resultSpan.innerHTML = eval(resultSpan.innerHTML).toFixed(2); //hitung sampai 2 angka dibelakang koma
-        }
-        if (clickSpan.innerHTML === 'C') {
-          resultSpan.innerHTML = ''; //clear
+        } else {
+          resultSpan.innerHTML += clickSpan.innerHTML; //tambahin string di belakang result
+          if (clickSpan.innerHTML === 'C') {
+            resultSpan.innerHTML = ''; //clear
+          }
         }
       } else {
-        if (operators.indexOf(last) > -1 || last === 'd') {
-          //kurangin angka plg belakang, kalo terakhirnya mod, 3 karakter yang ilang
+        if (operators.indexOf(last) > -1 || last === 'd') { //cek sebelumnya operator atau bukan, kalau ya ganti operator
           resultSpan.innerHTML = resultSpan.innerHTML.substring(0, resultSpan.innerHTML.length - (last === 'd' ? 3 : 1));
         }
-        //sudah males, hardcode aja yang bisa ngebugnya :)
-        if (clickSpan.innerHTML === 'mod' && last === 'd') {
-          resultSpan.innerHTML = resultSpan.innerHTML.substring(0, resultSpan.innerHTML.length - 3);
-        }
-        resultSpan.innerHTML += clickSpan.innerHTML; //tambahin string di belakang
-        if (resultSpan.innerHTML === '/' || resultSpan.innerHTML === 'x' || resultSpan.innerHTML === '^' || resultSpan.innerHTML === 'mod' || resultSpan.innerHTML == '%') {
+        resultSpan.innerHTML += clickSpan.innerHTML; //tambahin operator di belakang
+        if (operators.indexOf(clickSpan.innerHTML) > 1 && resultSpan.innerHTML == clickSpan.innerHTML) { //operator diawal tidak bisa. (selain + dan -)
           resultSpan.innerHTML = '';
-        }
+        } 
       }
     }
 });
